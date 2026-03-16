5be2c3c7207efeeed81849c614bd5d4ff78e69b4
charlotte <charlotte>
2026-03-16 19:47:03 +0100

    test add

--- PokeBlazor.Tests/UnitTest1.cs
+++ PokeBlazor.Tests/UnitTest1.cs
@@ -1,10 +1,44 @@
-﻿namespace PokeBlazor.Tests;
+﻿using System;
+using Xunit;
 
-public class UnitTest1
+namespace PokeBlazor.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test_String_IsNotNullOrEmpty()
+        {
+            string value = "Pikachu";
+            Assert.False(string.IsNullOrEmpty(value)); // Vérifie que la chaîne n'est pas vide
+        }
 
+        [Fact]
+        public void Test_Addition_Works()
+        {
+            int result = 2 + 3;
+            Assert.Equal(5, result); // Vérifie que 2 + 3 = 5
+        }
+
+        [Fact]
+        public void Test_String_Contains()
+        {
+            string name = "Charizard";
+            Assert.Contains("Char", name); // Vérifie que "Char" est bien dans la chaîne
+        }
+
+        [Fact]
+        public void Test_Exception_IsThrown()
+        {
+            Assert.Throws<ArgumentNullException>(() => throw new ArgumentNullException()); // Vérifie qu'une exception est bien levée
+        }
+
+        [Theory]
+        [InlineData(5, true)]
+        [InlineData(0, false)]
+        public void Test_IsPositive(int number, bool expected)
+        {
+            bool actual = number > 0;
+            Assert.Equal(expected, actual); // Vérifie si le nombre est positif ou non
+        }
     }
 }
